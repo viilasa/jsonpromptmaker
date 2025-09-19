ac2e32fc58692ac61ec73d9b01b1222ebf309682
viilasa <viilasacontact@gmail.com>
2025-09-20 03:40:26 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
-import postcssConfig from './postcss.config.js';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -17,7 +18,12 @@ export default defineConfig({
   },
   plugins: [react()],
   css: {
-    postcss: postcssConfig
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ]
+    }
   },
   resolve: {
     alias: {
